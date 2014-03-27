d7fb1b51be96f104505e91913ce322c71dda2ee6
Janick Martinez Esturo <janick@mpi-inf.mpg.de>
2014-03-27 21:35:44 +0100

    Add fish config based on oh-my-fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.local/pkg/oh-my-fish
+
+# Theme
+set fish_theme bobthefish
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+set SHELL /bin/fish
+set PATH $HOME/.local/bin $PATH
+alias config='git --git-dir=$HOME/.config.git/ --work-tree=$HOME'
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
