00753acdbcbc5ef8edfd2ea488af43223da2185f
Janick Martinez Esturo <janick@mpi-inf.mpg.de>
2014-03-28 20:27:51 +0100

    Switch to ph03 fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set fish_path $HOME/.local/pkg/oh-my-fish
 
 # Theme
-set fish_theme bobthefish
+set fish_theme ph03
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
