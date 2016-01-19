9721af2f40ebae76594ab94cb0db8d98bf0e6c27
Janick Martinez Esturo <janickm@nvidia.com>
2016-01-19 10:29:36 +0100

    Fix generic fish SHELL env variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ set fish_theme ph03
 # Example format: set fish_plugins autojump bundler
 
 # env vars
-set -x SHELL /usr/bin/fish
+set -x SHELL (which fish)
 set -x PATH $HOME/.local/bin $PATH
 set -x LD_LIBRARY_PATH $HOME/.local/lib $LD_LIBRARY_PATH
 set -x PROJVC $HOME/ProjVC
