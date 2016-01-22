94a34183c928591bc79843114ec909ca9553e61a
Janick Martinez Esturo <janickm@nvidia.com>
2016-01-22 10:59:56 +0100

    Update fish config
    - mc-wrapper for directory changes
    - markup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,10 @@
-# Path to your oh-my-fish.
+# Path to oh-my-fish repo
 set fish_path $HOME/.local/pkg/oh-my-fish
 
 # Theme
 set fish_theme ph03
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-
-# env vars
+# Env vars
 set -x SHELL (which fish)
 set -x PATH $HOME/.local/bin $PATH
 set -x LD_LIBRARY_PATH $HOME/.local/lib $LD_LIBRARY_PATH
@@ -17,22 +13,29 @@ set -x PROJMPI $HOME/ProjMPI
 set -x GCC_COLORS auto
 set -x NINJA_STATUS "[%r/%u/%f/%t] "
 
-# aliases
+# Aliases
 alias config='git --git-dir=$HOME/.config.git/ --work-tree=$HOME'
 
-# gem path
+function mc
+  set tmpfile /tmp/mcpwd-(random)
+  command mc -P $tmpfile
+  cd (cat $tmpfile)
+  rm $tmpfile
+end
+
+# Gem path
 if which gem20 ruby > /dev/null;
   set -x PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin $PATH;
 end
 
-# go path
+# Go path
 set -x GOPATH $HOME/.local/go
 set -x PATH $GOPATH/bin $PATH
 
-# rmk path
+# Rmk path
 set -x PATH $HOME/ProjVC/rmk/bin $PATH
 
-# rust path
+# Rust path
 set -x RUST_SRC_PATH="$HOME/.local/pkg/rust/src"
 
 # nvm-fish github.com/Alex7Kom/nvm-fish
@@ -41,7 +44,7 @@ test -s $HOME/.nvm-fish/nvm.fish; and source $HOME/.nvm-fish/nvm.fish
 # Path to your custom folder (default path is $FISH/custom)
 # set fish_custom $HOME/dotfiles/oh-my-fish
 
-# Load oh-my-fish configuration.
+# Load oh-my-fish configuration
 . $fish_path/oh-my-fish.fish
 
 # OPAM configuration
