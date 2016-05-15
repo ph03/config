38a8da49ad884cd6ae1531d86f86a977ac68d2e9
Janick Martinez Esturo <janickm@nvidia.com>
2016-05-15 16:41:59 +0200

    Remove `gd_enc_data_mount.sh` - malfunctioning because of google-drive-ocamlfuse

--- .local/bin/gd_enc_data_mount.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-~/.opam/system/bin/google-drive-ocamlfuse ~/Private/googledrive
-encfs --extpass='gksu -p -m EncFS_PW' $HOME/Private/googledrive/Private/encrypted $HOME/Private/gd_enc_data
