1884dfc52ff5009f234ffca38bedbf5d71038675
Janick Martinez Esturo <janickm@nvidia.com>
2021-05-27 13:59:45 +0200

    - general updates:   - don't show duration in fish commands   - update micro bindings   - update ignores + subl3 configs

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -3,4 +3,5 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme dark
 set -g theme_display_git_ahead_verbose yes
 set -g theme_display_date no
+set -g theme_display_cmd_duration no
 set -g theme_display_git_master_branch yes
